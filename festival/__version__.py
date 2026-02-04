c9aaa574f3a9ffcfc404a0cbc9afcee1be1765b5
Evan Yang <quantbit@126.com>
2026-02-04 16:29:23 +0800

    adding LICENSE.

--- festival/__version__.py
+++ festival/__version__.py
@@ -1,13 +1,13 @@
 __version__ = "0.0.0"
 __author__ = "aiwonderland"
-__copyright__ = "(C) 2026 aiwonderland and festival"
+__copyright__ = "(C) 2026 aiwonderland and festival team"
 __email__ = "quantbit@126.com"
 __description__ = "fun and enjoyment of terminal output"
-__date__ = "2026-2-03"
+__date__ = "2026-2-04"
 __contact__ = "aiwonderland <quantbit@126.com>"
 __all_author__ = ["aiwonderland"]
+__license__ = "MIT" 
 
 #Not yet
-__license__ = None  
 __url__ = None
 __download_url__ = None
